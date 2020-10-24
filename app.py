73f692e9e3699994a98e8484fdbf1a09fb1e3ee7
Amirul Islam <shiningflash007@gmail.com>
2020-10-24 14:04:00 +0600

    update project

---
+++ app.py
@@ -0,0 +1,45 @@
+
+import json
+import requests
+import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+
+URI = 'http://127.0.0.1:5000/'
+
+st.title('Neural Network Visualizer and Digit Prediction')
+st.sidebar.markdown('## Input Image')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+if st.sidebar.button('Get random prediction'):
+    response = requests.post(URI, data={})
+    response = json.loads(response.text)
+    preds = response.get('prediction')
+    image = response.get('image')
+    image = np.reshape(image, (28, 28))
+    st.sidebar.image(image, width = 150)
+    
+    for layer, p in enumerate(preds):
+        numbers = np.squeeze(np.array(p))
+        plt.figure(figsize = (32, 4))
+        
+        if layer == 2:
+            row = 1
+            col = 10
+        else:
+            row = 2
+            col = 16
+        
+        for i, number in enumerate(numbers):
+            plt.subplot(row, col, i + 1)
+            plt.imshow(number * np.ones((8, 8, 3)).astype('float32'))
+            plt.xticks([])
+            plt.yticks([])
+            
+            if layer == 2:
+                plt.xlabel(str(i), fontsize = 40)
+        
+        plt.subplots_adjust(wspace = 0.05, hspace = 0.05)
+        plt.tight_layout()
+        st.text('Layer {}'.format(layer + 1))
+        st.pyplot()
